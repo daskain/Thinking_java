169914bab078ce0b4924954fcd4b4b3d82ece00f
daskain <32793908+daskain@users.noreply.github.com>
2018-03-20 00:30:56 +0300

    Create task 9(max_value\min_value).java

---
+++ chapter 3/task 9(max_value\min_value).java
@@ -0,0 +1,15 @@
+import java.init.*;
+
+class Exp_value {
+	public static void main (String[] args){
+		
+		float f1 = Float.MAX_VALUE;
+		float f2 = Float.MIN_VALUE;
+		double d1 = Double.MAX_VALUE;
+		double d2 = Double.MIN_VALUE;
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println(d1);
+		System.out.println(d2);
+    }
+}
